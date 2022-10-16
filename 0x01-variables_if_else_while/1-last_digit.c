2f9394f26ed49473aa1cca22f1f873ff744f1ff9
mohmed diya <mohmedaiyaeldeen@gmail.com>
2022-10-16 07:47:17 +0300

    fix style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,9 +6,9 @@
 
 /**
  * main - Entry point
- * Description: Prints the last digit of random number and 
+ * Description: Prints the last digit of random number and
  * check if its 0 or greater than 5 or between 1-6
- * Rturn : Rturn 0
+ * Rturn : Always 0 (success)
 */
 int main(void)
 {
