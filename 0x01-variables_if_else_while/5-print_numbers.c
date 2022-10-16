afa46ea90e30e35756067d794cdc5ff7c5394b26
mohmed diya <mohmedaiyaeldeen@gmail.com>
2022-10-16 11:09:49 +0300

    program to print all digit from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main - Entry point
+ *
+ *Return : return 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}
