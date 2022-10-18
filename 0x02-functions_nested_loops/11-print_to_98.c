5b1fb57920bae7b401746c5ba70855ee1493dc71
mohmed diya <mohmedaiyaeldeen@gmail.com>
2022-10-18 16:48:27 +0300

    program that prints all the numbers until 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all numbers until 98
+ * @n: integer to start from
+ * Return: Return NONE
+*/
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (n; n <= 98; n++)
+		{
+			_putchar(n + ', ');
+		}
+	}
+	else if (n > 98)
+	{
+		for (n; n >= 98; n--)
+		{
+			_putchar(n+ ', ');
+		}
+	}
+}
