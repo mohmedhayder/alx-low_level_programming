cb26a5c73cdba2a288b87339f6877fa3ead5cdd3
mohmed diya <mohmedaiyaeldeen@gmail.com>
2022-10-18 16:34:16 +0300

    program to add two numbers and return result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers and return the result
+ * @first_number: first integer
+ * @second_number: second number
+ * Return: returns the result of addition 
+*/
+int add(int first_number, int second_number)
+{
+	int result = first_number + second_number;
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * Return: None
+*/
+void jack_bauer(void)
+{
+}
