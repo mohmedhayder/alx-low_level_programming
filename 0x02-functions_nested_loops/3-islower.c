ff56627baf0764cd7bba63169b71a7e6c6dcedb4
mohmed diya <mohmedaiyaeldeen@gmail.com>
2022-10-18 12:44:39 +0300

    add @c on des

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,7 @@
 
 /**
  * _islower - checks if the char lowercased or not
+ * @c: the value passed from outside the function
  * Return: if c is lowercas retrunt 1 else return 0
 */
 int _islower(int c)
