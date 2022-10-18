efad9c318d08318be9f89a8ab5d1bd6c0a0cb12c
mohmed diya <mohmedaiyaeldeen@gmail.com>
2022-10-18 16:37:43 +0300

    fix style

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,11 +3,12 @@
  * add - adds two integers and return the result
  * @first_number: first integer
  * @second_number: second number
- * Return: returns the result of addition 
+ * Return: returns the result of addition
 */
 int add(int first_number, int second_number)
 {
-	int result = first_number + second_number;
-	
+	int result;
+
+	result = first_number + second_number;
 	return (result);
 }
