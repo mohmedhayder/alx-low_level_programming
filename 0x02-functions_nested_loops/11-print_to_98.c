cb03cf003be051cc76b9f66408337d8b146e9cff
mohmed diya <mohmedaiyaeldeen@gmail.com>
2022-10-18 16:58:29 +0300

    fix logic

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,18 +7,20 @@
 */
 void print_to_98(int n)
 {
+	int i;
+
 	if (n < 98)
 	{
-		for (n; n < 98; n++)
+		for (i = n; i < 98; i++)
 		{
-			printf("%d, ", n);
+			printf("%d, ", i);
 		}
 	}
 	else if (n > 98)
 	{
-		for (n; n > 98; n--)
+		for (i =n ; i > 98; i--)
 		{
-			printf("%d, ", n);
+			printf("%d, ", i);
 		}
 	}
 	printf("98\n");
