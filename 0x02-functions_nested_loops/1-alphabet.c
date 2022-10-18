170a24fbd6f49804494253d0da61231faf58c6f0
mohmed diya <mohmedaiyaeldeen@gmail.com>
2022-10-18 11:30:47 +0300

    split the main to another file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,8 @@
 #include "main.h"
-
 /**
- * main - prints the alphabet in lowercase
- * Return: Always 0
+ * print_alphabet - prints the alphabet in lowercase
+ * Return: return 0
 */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 void print_alphabet(void)
 {
 	int letter;
@@ -19,4 +11,5 @@ void print_alphabet(void)
 		_putchar (letter);
 	}
 	_putchar('\n');
+	return (0);
 }
