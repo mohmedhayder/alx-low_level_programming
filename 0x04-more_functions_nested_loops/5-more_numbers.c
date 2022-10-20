d5c4bd70636ca4393206c1546bd2241316d94c6f
mohmed diya <mohmedaiyaeldeen@gmail.com>
2022-10-20 15:06:25 +0300

    program to print numbers from 0 to 14, 10 times followed by a new line

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14,
+ * 10 times followed by a new line
+*/
+
+void more_numbers(void)
+{
+	int i = 0, j;
+
+	while (i < 10)
+	{
+		int j = 0;
+		
+		while (j <= 14)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+		i++;
+	}
+}
