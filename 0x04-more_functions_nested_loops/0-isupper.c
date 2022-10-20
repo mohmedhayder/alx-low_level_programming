debb16a2bdf5b17721d4d57d2d878374e8f32129
mohmed diya <mohmedaiyaeldeen@gmail.com>
2022-10-21 01:21:43 +0300

    fix the style

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 /**
  * _isupper - Checks if the character in uppercase.
  * @c: charchter to be checked
- * Retuen: returns 1 if the charcter is upper case
+ * Return: returns 1 if the charcter is upper case
  * returns 0 otherwise
 */
 int _isupper(int c)
