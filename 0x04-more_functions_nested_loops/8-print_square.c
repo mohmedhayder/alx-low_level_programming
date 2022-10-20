ae8e8130394fbfc704f7f4e65e7bbbfb7bc15625
mohmed diya <mohmedaiyaeldeen@gmail.com>
2022-10-21 00:15:35 +0300

    program to print # numbers

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line
+*/
+
+void print_square(int size)
+{
+	int i = 0, j;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while(i < size)
+		{
+			j = 0;
+			
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
+}
