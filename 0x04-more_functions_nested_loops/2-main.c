867ad671c73f0152997e4f334c6bd941a1f97d20
mohmed diya <mohmedaiyaeldeen@gmail.com>
2022-10-20 14:32:17 +0300

    program to multiplie two integers

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks the code
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	printf("%d", mul(98, 1204));
+	printf("%d\n", mul(-402, 4096));
+
+	return (0);
+}
