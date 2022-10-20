88d8675f15799b1178f70121257f900f383ca3b0
mohmed diya <mohmedaiyaeldeen@gmail.com>
2022-10-20 11:56:22 +0300

    program to check if given charcter is in uppercase or not

---
+++ 0x04-more_functions_nested_loops/0-issuper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - checks if the character in uppercase
+ * @c: charchter to be checked
+ * Retuen: returns 1 if the charcter is upper case
+ * returns 0 otherwise
+*/
+
+int _isupper(int c)
+{
+	int i;
+	for (i >= 'A'; i <= 'Z'; i++)
+	{
+		if (c = i)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
