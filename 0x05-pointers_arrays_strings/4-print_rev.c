f9f4a9963beac166836b091442b3d8d42029e326
mohmed diya <mohmedaiyaeldeen@gmail.com>
2022-10-25 00:14:21 +0300

    fix the logic

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse.
+ * @s: the string to print in reverse.
+*/
+
+void print_rev(char *s)
+{
+	int len = 0, index = 0;
+	len = _strlen(s);
+
+	for (index = len - 1; index >= 0; index--)
+	{
+		_putchar(s[index]);
+	}
+	_putchar('\n');
+}
