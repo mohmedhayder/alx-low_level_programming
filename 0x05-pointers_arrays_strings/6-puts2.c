d956595f472cc50dac5210a907f6eace987496f1
mohmed diya <mohmedaiyaeldeen@gmail.com>
2022-10-25 00:56:22 +0300

    program prints part of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints part of a stiring
+ * @str: stirng to be printed
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
