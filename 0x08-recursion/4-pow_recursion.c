d710f4a9992cf21be7ae5d52ecea36b76d4a7d40
mohmed diya <mohmedaiyaeldeen@gmail.com>
2022-11-03 00:26:44 +0300

    fix the style

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _pow_recrsion - function that returns the
+ * _pow_recursion - function that returns the
  * @x: the base
  * @y: the exponent
  * Return: the value of x raised to the power of y
