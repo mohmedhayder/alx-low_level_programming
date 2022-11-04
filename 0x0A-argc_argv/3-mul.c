e62098cc0e5917743c620e04ae7ec96f471d0b02
mohmed diya <mohmedaiyaeldeen@gmail.com>
2022-11-04 16:58:31 +0300

    fix logic

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - prints the program name
  * @argc: number of args
@@ -12,7 +13,7 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-		result = (ation(argv[1]) * ation(argv[2]));
+		result = (atoi(argv[1]) * atoi(argv[2]));
 		printf("%d\n", result);
 		return (0);
 	}
