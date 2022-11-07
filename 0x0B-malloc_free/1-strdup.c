6937a1c5598f4a99a4146323d98e0f49623b6156
mohmed diya <mohmedaiyaeldeen@gmail.com>
2022-11-07 23:39:22 +0300

    fix malloc location

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,8 +19,8 @@ char *_strdup(char *str)
 	for (index = 0; str[index]; index++)
 	{
 		len++;
-		cpy = malloc(sizeof(char) * (len + 1));
 	}
+	cpy = malloc(sizeof(char) * (len + 1));
 	if (cpy == NULL)
 	{
 		return (NULL);
