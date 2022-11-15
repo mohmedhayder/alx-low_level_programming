1ba1dfce6b2eeeba01cf8de3f40437c825a1df99
mohmed diya <mohmedaiyaeldeen@gmail.com>
2022-11-15 09:20:12 +0300

    fix logic

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	}
 	if ((*d).name == NULL)
 	{
-		printf("nil\n");
+		printf("Name: (nil)\n");
 	}
 	else
 	{
@@ -21,7 +21,7 @@ void print_dog(struct dog *d)
 	}
 	if ((*d).age < 0)
 	{
-		printf("nil\n");
+		printf("Age: (nil)\n");
 	}
 	else
 	{
@@ -29,7 +29,7 @@ void print_dog(struct dog *d)
 	}
 	if ((*d).owner == NULL)
 	{
-		printf("nil\n");
+		printf("Owner: (nil)\n");
 	}
 	else
 	{
